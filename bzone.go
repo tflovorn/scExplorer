a6bd759b55d6ef7729a7f27a4469b74941f93c15
Tim Lovorn <tflovorn@crimson.ua.edu>
2012-07-11 17:42:18 -0500

    Partially implement bzPoints

--- bzone.go
+++ bzone.go
@@ -13,6 +13,7 @@ func BzSum(pointsPerSide int64, dimension int64, fn BzFunc) float64 {
 	c := 0.0
 	var y, t float64
 	add := func(next float64, total *float64) {
+		// add next to total; c holds error compensation information
 		y = next - c
 		t = *total + y
 		c = (t - *total) - y
@@ -31,18 +32,17 @@ func BzMinimum(pointsPerSide int64, dimension int64, fn BzFunc) float64 {
 	return bzReduce(minimum, math.MaxFloat64, pointsPerSide, dimension, fn)
 }
 
-// Iterate over the Brillouin zone, accumulating the values of fn with cs.
-func bzReduce(cs bzConsumer, start float64, pointsPerSide int64, dimension int64, fn BzFunc) float64 {
+// Iterate over the Brillouin zone, accumulating the values of fn with combine.
+func bzReduce(combine bzConsumer, start float64, pointsPerSide int64, dimension int64, fn BzFunc) float64 {
 	maxWorkers := 2 // TODO: set this via config file
 	points, done := bzPoints(pointsPerSide, dimension)
-	results := make([]chan float64, maxWorkers)
 	work := func(result chan float64) {
 		var k BzPoint
 		total := start
 		for {
 			select {
 			case k = <-points:
-				cs(fn(k), &total)
+				combine(fn(k), &total)
 			case <-done:
 				done <- true
 				break
@@ -50,14 +50,16 @@ func bzReduce(cs bzConsumer, start float64, pointsPerSide int64, dimension int64
 		}
 		result <- total
 	}
-
+	// set up workers
+	results := make([]chan float64, maxWorkers)
 	for i := 0; i < maxWorkers; i++ {
 		results[i] = make(chan float64)
 		go work(results[i])
 	}
+	// collect the results
 	fullTotal := start
 	for i := 0; i < maxWorkers; i++ {
-		cs(<-results[i], &fullTotal)
+		combine(<-results[i], &fullTotal)
 	}
 	return fullTotal
 }
@@ -66,5 +68,27 @@ func bzReduce(cs bzConsumer, start float64, pointsPerSide int64, dimension int64
 // Brillouin zone point once, and done is is a channel which contains true
 // after all points have been traversed.
 func bzPoints(pointsPerSide int64, dimension int64) (chan BzPoint, chan bool) {
-	return nil, nil
+	points := make(chan BzPoint)
+	done := make(chan bool)
+	// start is the minumum value of any component of a point
+	start := -math.Pi
+	// (finish - step) is the maximum value of any component of a point
+	finish := -start
+	// separation between point components
+	step := (finish - start) / float64(pointsPerSide)
+
+	generatePoints := func() {
+		k := make([]float64, dimension)
+		for i := int64(0); i < dimension; i++ {
+			k[i] = start
+		}
+		for {
+			// TODO: iterate over points
+			k[0] += step
+			break
+		}
+		done <- true
+	}
+	go generatePoints()
+	return points, done
 }
