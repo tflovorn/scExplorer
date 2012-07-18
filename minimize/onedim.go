f68cf8912443a3b18f215c0bac29637e9affbc2b
Tim Lovorn <tflovorn@crimson.ua.edu>
2012-07-18 13:56:27 -0500

    Abandon minimizing in favor of root-finding.
    Will use GSL multidimensional algorithms (see ch. 35 of GSL manual).

--- minimize/onedim.go
+++
@@ -1,9 +0,0 @@
-// One-dimensional inexact minimization. Based on:
-// http://en.wikipedia.org/wiki/Backtracking_line_search
-// http://en.wikipedia.org/wiki/Wolfe_conditions
-package minimize
-
-// Find the minimum of fn on the interval [a, b].
-func OneDimMin(fn func(float64) float64, a, b float64) (float64, error) {
-	return 0.0, nil
-}
