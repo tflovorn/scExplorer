d467978333ee07fa3cde03c76b089264007a1486
Tim Lovorn <tflovorn@crimson.ua.edu>
2012-07-29 23:48:59 -0500

    add tests for autodiff.go

--- solve/autodiff.go
+++ solve/autodiff.go
@@ -10,7 +10,8 @@ static int centralDeriv(void *uservar, double x, double h, double *result, doubl
 	gsl_function F;
 	F.function = &go_val;
 	F.params = uservar;
-	return gsl_deriv_central(&F, x, h, result, abserr);
+	int err = gsl_deriv_central(&F, x, h, result, abserr);
+	return err;
 }
 */
 import "C"
@@ -31,16 +32,40 @@ type fnWithIndex struct {
 // epsabs. h is the initial step size.
 func Derivative(fn vec.FnDim0, v vec.Vector, i int, h, epsabs float64) (float64, error) {
 	iters, maxIters := 0, 100
+	// results are bad for h too small or too large; we iterate in both directions
+	hMin := h / 1e6
+	hMax := h * 1e6
+	hRising := true
+	hInitial := h
+	hOk := func(h float64) bool {
+		if !hRising && h > hMin {
+			return true
+		} else if hRising && h < hMax {
+			return true
+		}
+		return false
+	}
+	hAdvance := func(h float64) float64 {
+		if hRising {
+			if h*2.0 > hMax {
+				hRising = false
+				return hInitial / 2.0
+			}
+			return h * 2.0
+		}
+		return h / 2.0
+	}
 	fwi := unsafe.Pointer(&fnWithIndex{fn, v, i})
 	x := C.double(v[i])
 	result, abserr := C.double(0.0), C.double(math.MaxFloat64)
-	for float64(abserr) > epsabs && iters < maxIters {
+	for float64(abserr) > epsabs && iters < maxIters && hOk(h) {
 		err := C.centralDeriv(fwi, x, C.double(h), &result, &abserr)
 		if err != C.GSL_SUCCESS {
 			err_str := C.GoString(C.gsl_strerror(err))
 			return float64(result), fmt.Errorf("error in Derivative (GSL): %v\n", err_str)
 		}
 		iters++
+		h = hAdvance(h)
 	}
 	return float64(result), nil
 }
---
+++ solve/autodiff_test.go
@@ -0,0 +1,45 @@
+package solve
+
+import (
+	"math"
+	"testing"
+)
+import vec "../vector"
+
+// Check if the derivative of a*v is a constant.
+func TestAutoDiffLinear(t *testing.T) {
+	a := 5.0
+	fn := func(v vec.Vector) (float64, error) {
+		return a*v[0] + a*v[1], nil
+	}
+	v := []float64{1.0, 1.0}
+	h := 1e-4
+	epsabs := 1e-8
+	deriv, err := Derivative(fn, v, 0, h, epsabs)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if math.Abs(deriv-a) > epsabs {
+		t.Fatalf("linear derivative not equal to constant (got %v, expected %v)", deriv, a)
+	}
+}
+
+// Check if the derivative of a*v*v is 2*a*v.
+func TestAutoDiffSquare(t *testing.T) {
+	a := 5.0
+	fn := func(v vec.Vector) (float64, error) {
+		return a*v[0]*v[0] + a*v[1]*v[1], nil
+	}
+	v := []float64{1.0, 1.0}
+	v0 := v[0]
+	h := 1e-4
+	epsabs := 1e-8
+	deriv, err := Derivative(fn, v, 0, h, epsabs)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := 2.0 * a * v0
+	if math.Abs(deriv-expected) > epsabs {
+		t.Fatalf("linear derivative not equal to constant (got %v, expected %v)", deriv, expected)
+	}
+}
