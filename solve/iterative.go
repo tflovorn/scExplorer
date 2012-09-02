c3f8c5291c6d5bfb33b98083828761de4dbda592
Tim Lovorn <tflovorn@crimson.ua.edu>
2012-09-02 16:31:26 -0500

    add solve.Iterative: multi-stage solver

---
+++ solve/iterative.go
@@ -0,0 +1,51 @@
+package solve
+
+import "math"
+import (
+	vec "../vector"
+)
+
+// Solve each system in `stages` iteratively, starting with stages[0] and
+// proceeding upward. After solving each stage, check if all previous stages
+// are still solved. If they are not, return to stages[0]. `accept` should
+// take a []Vector representing the current state of each stage and store it;
+// this allows the stages to be coupled.
+func Iterative(stages []DiffSystem, start []vec.Vector, epsAbs, epsRel []float64, accept func([]vec.Vector)) ([]vec.Vector, error) {
+	x := start
+	for !allSolved(x, stages, epsAbs, len(stages)-1) {
+		for i, system := range stages {
+			var err error
+			x[i], err = MultiDim(system, x[i], epsAbs[i], epsRel[i])
+			if err != nil {
+				return nil, err
+			}
+			accept(x)
+			if !allSolved(x, stages, epsAbs, i-1) {
+				break
+			}
+		}
+	}
+	return x, nil
+}
+
+// Check if `x` solves all stages with index <= `n`
+func allSolved(x []vec.Vector, stages []DiffSystem, epsAbs []float64, n int) bool {
+	if n < 0 {
+		return true
+	}
+	for i, system := range stages {
+		if i > n {
+			break
+		}
+		absErr, err := system.F(x[i])
+		if err != nil {
+			return false
+		}
+		for _, e := range absErr {
+			if math.Abs(e) > epsAbs[i] {
+				return false
+			}
+		}
+	}
+	return true
+}
---
+++ solve/iterative_test.go
@@ -0,0 +1,46 @@
+package solve
+
+import (
+	"testing"
+	"math"
+)
+import vec "../vector"
+
+func TestIterativeRosenbrock(t *testing.T) {
+	epsAbs := []float64{1e-9, 1e-9}
+	epsRel := []float64{1e-9, 1e-9}
+	expected := []vec.Vector{[]float64{1.0}, []float64{1.0}}
+	start := []vec.Vector{[]float64{10.0}, []float64{5.0}}
+	stages, accept := rosenbrockSystems(1.0, 10.0, start)
+	solutions, err := Iterative(stages, start, epsAbs, epsRel, accept)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, vec := range expected {
+		for j, val := range vec {
+			if math.Abs(solutions[i][j] - val) > epsAbs[i] {
+				t.Fatalf("incorrect Rosenbrock(1.0, 1.0) solution: %v", solutions)
+			}
+		}
+	}
+}
+
+func rosenbrockSystems(a, b float64, start []vec.Vector) ([]DiffSystem, func([]vec.Vector)) {
+	var x1, x2 float64
+	accept := func(x []vec.Vector) {
+		x1 = x[0][0]
+		x2 = x[1][0]
+	}
+	accept(start)
+	f1 := func(v vec.Vector) (float64, error) {
+		return a * (1.0 - v[0]), nil
+	}
+	f2 := func(v vec.Vector) (float64, error) {
+		return b * (v[0] - x1*x1), nil
+	}
+	d1 := SimpleDiffable(f1, 1, 1e-4, 1e-9)
+	d2 := SimpleDiffable(f2, 1, 1e-4, 1e-9)
+	s1 := Combine([]Diffable{d1})
+	s2 := Combine([]Diffable{d2})
+	return []DiffSystem{s1, s2}, accept
+}
