64761ae43062be0f6d9b1a6b55d8854c898db067
Tim Lovorn <tflovorn@crimson.ua.edu>
2013-10-12 18:55:53 -0500

    interface to GSL implementation of Levin u-transform

---
+++ seriesaccel/levin.go
@@ -0,0 +1,57 @@
+package seriesaccel
+
+/*
+#cgo LDFLAGS: -lgsl -lgslcblas
+#include <stdio.h>
+#include <stdlib.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_sum.h>
+extern void levin_go_f(int, void *, double *);
+
+static void levin(void * fn, int iStart, int Nterms, double * result, double * absErr, int * terms) {
+	gsl_sum_levin_u_workspace * w = gsl_sum_levin_u_alloc(Nterms);
+	double * t = malloc(Nterms * sizeof(double));
+	int i;
+
+	for (i = 0; (i - iStart) < Nterms; i++) {
+		double val = 0.0;
+		levin_go_f(i, fn, &val);
+		t[i] = val;
+	}
+
+	gsl_sum_levin_u_accel(t, Nterms, w, result, absErr);
+	*terms = w->terms_used;
+
+	free(t);
+	gsl_sum_levin_u_free(w);
+}
+*/
+import "C"
+
+import "unsafe"
+
+type TermFn func(int) float64
+
+// Evaluate the series \sum_{i=iStart}^{\infty}[termFn(i)] using the
+// Levin u-transform as provided by GSL. Returns the sum and error estimate.
+// The initial evaluation will use Nstart terms. The series acceleration
+// function uses as many as are necessary to minimize the error; if all terms
+// are consumed, the number of terms used will be increased.
+func Levin_u(fn TermFn, iStart, Nstart int) (float64, float64) {
+	result, absErr := C.double(0.0), C.double(0.0)
+	terms := C.int(0)
+
+	C.levin(unsafe.Pointer(&fn), C.int(iStart), C.int(Nstart), &result, &absErr, &terms)
+
+	// if number of terms used == Nstart, evaluate again with larger Nstart
+	if int(terms) == Nstart {
+		return Levin_u(fn, iStart, Nstart*2)
+	}
+	return float64(result), float64(absErr)
+}
+
+//export levin_go_f
+func levin_go_f(i C.int, fn unsafe.Pointer, result *C.double) {
+	gofn := *((*TermFn)(fn))
+	*result = C.double(gofn(int(i)))
+}
