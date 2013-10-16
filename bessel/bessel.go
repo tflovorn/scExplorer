1f3a3f5b1f6cbe46fdd04726b28badc7238f22b2
Tim Lovorn <tflovorn@crimson.ua.edu>
2013-10-15 21:43:43 -0500

    interface to GSL calculation of Bessel I1

--- bessel/bessel.go
+++ bessel/bessel.go
@@ -7,6 +7,13 @@ package bessel
 */
 import "C"
 
+// Zeroth-order modified Bessel function of the first kind (I0)
 func ModifiedBesselFirstKindZeroth(x float64) float64 {
 	return float64(C.gsl_sf_bessel_I0(C.double(x)))
+}
+
+// First-order modified Bessel function of the first kind (I1)
+func ModifiedBesselFirstKindFirst(x float64) float64 {
+	return float64(C.gsl_sf_bessel_I1(C.double(x)))
+
 }
--- bessel/bessel_test.go
+++ bessel/bessel_test.go
@@ -5,13 +5,23 @@ import (
 )
 
 func TestModifiedBesselFirstKindZeroth(t *testing.T) {
-	if ModifiedBesselFirstKindZeroth(0.0) != 1.0 {
-		t.Error("incorrect Bessel function value for I0(0.0)")
+	input := []float64{0.0, 2.0, 3.0}
+	expected := []float64{1.0, 2.2795853023360673, 4.880792585865024}
+	for i, val := range(input) {
+		b := ModifiedBesselFirstKindZeroth(val)
+		if b != expected[i] {
+			t.Errorf("incorrect Bessel function value for I0(%f); got %v, expected %v", val, b, expected[i])
+		}
 	}
-	if ModifiedBesselFirstKindZeroth(2.0) != 2.2795853023360673 {
-		t.Error("incorrect Bessel function value for I0(2.0)")
-	}
-	if ModifiedBesselFirstKindZeroth(3.0) != 4.880792585865024 {
-		t.Error("incorrect Bessel function value for I0(3.0)")
+}
+
+func TestModifiedBesselFirstKindFirst(t *testing.T) {
+	input := []float64{0.0, 2.0, 3.0}
+	expected := []float64{0.0, 1.590636854637329, 3.9533702174026093}
+	for i, val := range(input) {
+		b := ModifiedBesselFirstKindFirst(val)
+		if b != expected[i] {
+			t.Errorf("incorrect Bessel function value for I0(%f); got %v, expected %v", val, b, expected[i])
+		}
 	}
 }
