134d32ba9052f4b589f2fc363004cf1ff2d20d3e
Tim Lovorn <tflovorn@crimson.ua.edu>
2013-11-01 00:22:28 -0500

    update energy_test.go to refer to tempCrit

--- tempFluc/energy_test.go
+++ tempFluc/energy_test.go
@@ -3,6 +3,7 @@ package tempFluc
 import (
 	"testing"
 )
+import "../tempCrit"
 
 func TestEnergies(t *testing.T) {
 	// kz^2 values
@@ -19,14 +20,14 @@ func TestEnergies(t *testing.T) {
 	env.D1 = defaultEnvSolution[0]
 	env.Mu_h = defaultEnvSolution[1]
 	env.Beta = defaultEnvSolution[2]
-	holon, err := HolonEnergy(env)
+	holon, err := tempCrit.HolonEnergy(env)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if holon != expectedHolon {
 		t.Fatalf("unexpected holon energy value %v (expected %v)", holon, expectedHolon)
 	}
-	pair, err := PairEnergy(env)
+	pair, err := tempCrit.PairEnergy(env)
 	if err != nil {
 		t.Fatal(err)
 	}
