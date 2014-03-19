5fb0ad4ddeb89c6f85417ef39608a35b139cf2f8
Tim Lovorn <tflovorn@crimson.ua.edu>
2014-03-19 03:58:11 -0500

    add production plots to tempPair

--- tempPair/system_test.go
+++ tempPair/system_test.go
@@ -2,10 +2,10 @@ package tempPair
 
 import (
 	"flag"
-	"fmt"
+	//"fmt"
 	"io/ioutil"
 	"os"
-	"strconv"
+	//"strconv"
 	"testing"
 )
 import (
@@ -13,6 +13,8 @@ import (
 	"../tempAll"
 )
 
+var production = flag.Bool("production", false, "Production mode: make plots that shouldn't change.")
+var printerPlots = flag.Bool("printerPlots", false, "Use line types as plot styles instead of colors.")
 var testPlot = flag.Bool("testPlot", false, "Run tests involving plots")
 var longPlot = flag.Bool("longPlot", false, "Run long version of plot tests")
 var tinyX = flag.Bool("tinyX", false, "Plot very small values of X")
@@ -44,10 +46,50 @@ func ptDefaultEnv() (*tempAll.Environment, error) {
 	return env, nil
 }
 
+// Production-ready plots:
+//   D1, Mu_h, and Tp vs x.
+//   Extra set of plots to zoom in on x=0.
+//   Vary tz and thp independently in each set (fix one at 0.1 and the other is in {-0.1, 0.0, 0.1}).
+func TestProductionPlots(t *testing.T) {
+	flag.Parse()
+	if !*production {
+		return
+	}
+	defaultEnv, err := ptDefaultEnv()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// vary thp
+	envs := defaultEnv.MultiSplit([]string{"X", "Tz", "Thp"}, []int{30, 1, 3}, []float64{0.01, 0.1, -0.1}, []float64{0.15, 0.1, 0.1})
+	fileLabelTp := "plot_data_THP.tp_x"
+	fileLabelMu := "plot_data_THP.mu_x"
+	fileLabelD1 := "plot_data_THP.D1_x"
+	eps := 1e-9
+	err = solveAndPlot(envs, eps, eps, fileLabelTp, fileLabelMu, fileLabelD1)
+	// vary thp (small x)
+	envs = defaultEnv.MultiSplit([]string{"X", "Tz", "Thp"}, []int{30, 1, 3}, []float64{0.001, 0.1, -0.1}, []float64{0.01, 0.1, 0.1})
+	fileLabelTp = "plot_data_THP_LOWX.tp_x"
+	fileLabelMu = "plot_data_THP_LOWX.mu_x"
+	fileLabelD1 = "plot_data_THP_LOWX.D1_x"
+	err = solveAndPlot(envs, eps, eps, fileLabelTp, fileLabelMu, fileLabelD1)
+	// vary tz
+	envs = defaultEnv.MultiSplit([]string{"X", "Tz", "Thp"}, []int{30, 3, 1}, []float64{0.01, -0.1, 0.1}, []float64{0.15, 0.1, 0.1})
+	fileLabelTp = "plot_data_TZ.tp_x"
+	fileLabelMu = "plot_data_TZ.mu_x"
+	fileLabelD1 = "plot_data_TZ.D1_x"
+	err = solveAndPlot(envs, eps, eps, fileLabelTp, fileLabelMu, fileLabelD1)
+	// vary tz (small x)
+	envs = defaultEnv.MultiSplit([]string{"X", "Tz", "Thp"}, []int{30, 3, 1}, []float64{0.001, -0.1, 0.1}, []float64{0.01, 0.1, 0.1})
+	fileLabelTp = "plot_data_TZ_LOWX.tp_x"
+	fileLabelMu = "plot_data_TZ_LOWX.mu_x"
+	fileLabelD1 = "plot_data_TZ_LOWX.D1_x"
+	err = solveAndPlot(envs, eps, eps, fileLabelTp, fileLabelMu, fileLabelD1)
+}
+
 // Plot evolution of Tp vs X.
 func TestPlotTpVsX(t *testing.T) {
 	flag.Parse()
-	if !*testPlot {
+	if !*testPlot || *production {
 		return
 	}
 	defaultEnv, err := ptDefaultEnv()
@@ -96,4 +138,53 @@ func TestPlotTpVsX(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error making D1 plot: %v", err)
 	}
+}
+
+func solveAndPlot(envs []*tempAll.Environment, epsabs, epsrel float64, fileLabelTp, fileLabelMu, fileLabelD1 string) error {
+	// solve
+	plotEnvs, errs := tempAll.MultiSolve(envs, epsabs, epsrel, PairTempSolve)
+	// T_p vs x plot
+	vars := plots.GraphVars{"X", "", []string{"Tz", "Thp"}, []string{"t_z", "t_h^{\\prime}"}, nil, tempAll.GetTemp}
+	wd, _ := os.Getwd()
+	grapherPath := wd + "/../plots/grapher.py"
+	graphParams := map[string]string{plots.FILE_KEY: wd + "/" + fileLabelTp, plots.XLABEL_KEY: "$x$", plots.YLABEL_KEY: "$T_p$", plots.YMIN_KEY: "0.0"}
+	err := plots.MultiPlotStyle(plotEnvs, errs, vars, graphParams, grapherPath, false)
+	if err != nil {
+		return err
+	}
+	graphParams[plots.FILE_KEY] = wd + "/" + fileLabelTp + "_BW_"
+	err = plots.MultiPlotStyle(plotEnvs, errs, vars, graphParams, grapherPath, true)
+	if err != nil {
+		return err
+	}
+	// Mu_h vs x plot
+	graphParams[plots.FILE_KEY] = wd + "/" + fileLabelMu
+	graphParams[plots.YLABEL_KEY] = "$\\mu_h$"
+	graphParams[plots.YMIN_KEY] = ""
+	vars.Y = "Mu_h"
+	vars.YFunc = nil
+	err = plots.MultiPlotStyle(plotEnvs, errs, vars, graphParams, grapherPath, false)
+	if err != nil {
+		return err
+	}
+	graphParams[plots.FILE_KEY] = wd + "/" + fileLabelMu + "_BW_"
+	err = plots.MultiPlotStyle(plotEnvs, errs, vars, graphParams, grapherPath, true)
+	if err != nil {
+		return err
+	}
+	// D1 vs x plot
+	graphParams[plots.FILE_KEY] = wd + "/" + fileLabelD1
+	graphParams[plots.YLABEL_KEY] = "$D_1$"
+	vars.Y = "D1"
+	vars.YFunc = nil
+	err = plots.MultiPlotStyle(plotEnvs, errs, vars, graphParams, grapherPath, false)
+	if err != nil {
+		return err
+	}
+	graphParams[plots.FILE_KEY] = wd + "/" + fileLabelD1 + "_BW_"
+	err = plots.MultiPlotStyle(plotEnvs, errs, vars, graphParams, grapherPath, true)
+	if err != nil {
+		return err
+	}
+	return nil
 }
