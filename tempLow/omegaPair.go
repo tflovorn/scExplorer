2563859c5ce190719a3e373eff4b9c23eaf289b3
Tim Lovorn <tflovorn@crimson.ua.edu>
2014-08-14 22:18:04 -0500

    Driving anomalous coefficients near 0 at Tc.
    Not quite at 0 due to differences in fitting procedure (?).
    Key change: solving (1-lambda)^2 = 0 requires a smaller tolerance than
    solving (1-lambda) = 0. So solve sqrt(abs(1-lambda)^2) instead.

--- tempLow/omegaPair.go
+++ tempLow/omegaPair.go
@@ -37,7 +37,11 @@ func LambdaFn(env *tempAll.Environment, k vec.Vector, r, s int) func(float64) (f
 		Re_MD := u + float64(r)*v
 		u_anom, v_anom := parts_MDiagAnom(env, k, omega)
 		MDA := u_anom + float64(r)*v_anom
-		result := math.Pow(Re_MD, 2.0) - math.Pow(MDA, 2.0)
+		//fmt.Printf("u = %v, v = %v, uA = %v, vA = %v\n", u, v, u_anom, v_anom)
+		//uTc, vTc := tempCrit.LambdaParts(env, k, omega)
+		//fmt.Printf("uTc = %v, vTc = %v\n", uTc, vTc)
+		//fmt.Printf("u + v = %v; 1 - (uTc + vTc) = %v\n", u + v, 1.0 - (uTc + vTc))
+		result := math.Sqrt(math.Abs(math.Pow(Re_MD, 2.0) - math.Pow(MDA, 2.0)))
 		//fmt.Printf("for omega=%f got lambda=%f\n", omega, result)
 		return result, nil
 	}
@@ -49,21 +53,19 @@ func parts_MDiag(env *tempAll.Environment, k vec.Vector, omega float64) (float64
 	Ey := 2.0 * (env.T0*cx + env.Tz*cz)
 	Pis := Pi(env, []float64{k[0], k[1]}, omega) // Pi_{xx, xy, yy}
 	//fmt.Printf("for omega=%f got Pis=%v\n", omega, Pis)
-	u := -0.25 * (-(1.0/Ex + 1.0/Ey) + Pis[0] + Pis[2])
-	v := -0.5 * math.Abs(Pis[1])
+	u := -0.25 * (Ex*Pis[0] + Ey*Pis[2] - 2.0)
+	v := -0.5 * math.Sqrt(Ex*Ey) * math.Abs(Pis[1])
 	return u, v
 }
 
 func parts_MDiagAnom(env *tempAll.Environment, k vec.Vector, omega float64) (float64, float64) {
-	//cx, cy, cz := math.Cos(k[0]), math.Cos(k[1]), math.Cos(k[2])
-	//Ex := 2.0 * (env.T0*cy + env.Tz*cz)
-	//Ey := 2.0 * (env.T0*cx + env.Tz*cz)
+	cx, cy, cz := math.Cos(k[0]), math.Cos(k[1]), math.Cos(k[2])
+	Ex := 2.0 * (env.T0*cy + env.Tz*cz)
+	Ey := 2.0 * (env.T0*cx + env.Tz*cz)
 	PiAs_plus := PiAnom(env, []float64{k[0], k[1]}, omega) // Pi^A_{xx, xy, yy}
 	PiAs_minus := PiAnom(env, []float64{-k[0], -k[1]}, -omega)
-	//fmt.Printf("for omega=%f got PiAs_plus=%v\n", omega, PiAs_plus)
-	//fmt.Printf("for omega=%f got PiAs_minus=%v\n", omega, PiAs_minus)
-	u := -0.25 * (PiAs_plus[0] + PiAs_minus[0] + PiAs_plus[2] + PiAs_minus[2])
-	v := -0.5 * math.Abs(PiAs_plus[1] + PiAs_minus[1])
+	u := -0.25 * (Ex*PiAs_plus[0] + Ex*PiAs_minus[0] + Ey*PiAs_plus[2] + Ey*PiAs_minus[2])
+	v := -0.5 * math.Sqrt(Ex*Ey) * math.Abs(PiAs_plus[1] + PiAs_minus[1])
 	return u, v
 }
 
@@ -94,7 +96,7 @@ func omegaFitHelper(env *tempAll.Environment, fn tempCrit.OmegaFunc, points_qx, 
 			fmt.Printf("at k=%v got err=%v\n", q, err)
 			continue
 		}
-		fmt.Printf("at k=%v got omega=%f\n", q, omega)
+		//fmt.Printf("at k=%v got omega=%v\n", q, omega)
 		used_points_qx = append(used_points_qx, q)
 		omegas_qx = append(omegas_qx, omega)
 	}
@@ -103,14 +105,14 @@ func omegaFitHelper(env *tempAll.Environment, fn tempCrit.OmegaFunc, points_qx, 
 		if err != nil {
 			continue
 		}
-		fmt.Printf("at k=%v got omega=%f\n", q, omega)
+		//fmt.Printf("at k=%v got omega=%v\n", q, omega)
 		used_points_qz = append(used_points_qz, q)
 		omegas_qz = append(omegas_qz, omega)
 	}
 	if len(omegas_qx) < 3 || len(omegas_qz) < 3 {
 		return nil, fmt.Errorf("not enough omega_+/- values can be found")
 	}
-	fmt.Printf("omegas_qx = %v\n", omegas_qx)
+	//fmt.Printf("omegas_qx = %v\n", omegas_qx)
 	// difference between fit and omega_i
 	errFuncF_qx := func(cs vec.Vector, i int) (float64, error) {
 		//fmt.Printf("in qx trying coeffs=%v\n", cs)
@@ -127,7 +129,7 @@ func omegaFitHelper(env *tempAll.Environment, fn tempCrit.OmegaFunc, points_qx, 
 		} else {
 			result = make([]float64, 2)
 			result[0] = 2.0 * (cs[0] * qx2 - cs[1]) * qx2
-			result[1] = 2.0 * (env.A - cs[1]) * qx2
+			result[1] = 2.0 * (env.A - cs[0]) * qx2
 		}
 		return result, nil
 	}
@@ -141,14 +143,14 @@ func omegaFitHelper(env *tempAll.Environment, fn tempCrit.OmegaFunc, points_qx, 
 	if err != nil {
 		return coeffs_qx, err
 	}
-	fmt.Printf("---got qx coeffs=%v\n", coeffs_qx)
+	//fmt.Printf("---got qx coeffs=%v\n", coeffs_qx)
 	if env.B == 0.0 {
 		guess_qz = []float64{env.Tz, 0.001}
 	} else {
 		guess_qz = []float64{0.001}
 	}
 	errFuncF_qz := func(cs vec.Vector, i int) (float64, error) {
-		fmt.Printf("in qz trying coeffs=%v\n", cs)
+		//fmt.Printf("in qz trying coeffs=%v\n", cs)
 		if env.A == 0.0 {
 			return math.Pow(omegas_qz[i], 2.0) - OmegaFromFit_qz(cs, used_points_qz[i], coeffs_qx[2], env.B), nil
 		} else {
@@ -168,13 +170,13 @@ func omegaFitHelper(env *tempAll.Environment, fn tempCrit.OmegaFunc, points_qx, 
 		}
 		return result, nil
 	}
-	fmt.Printf("guess_qz = %v; used_points_qz = %v\n", guess_qz, used_points_qz)
+	//fmt.Printf("guess_qz = %v; used_points_qz = %v\n", guess_qz, used_points_qz)
 	coeffs_qz, err := fit.MultiDim(errFuncF_qz, errFuncDf_qz, len(used_points_qz), guess_qz, epsabs, epsrel)
 	if err != nil {
 		return coeffs_qz, err
 	}
-	fmt.Printf("---got q coeffs=%v\n", coeffs_qz)
-	fmt.Printf("coeffs_qx: %v; coeffs_qz: %v\n", coeffs_qx, coeffs_qz)
+	//fmt.Printf("---got q coeffs=%v\n", coeffs_qz)
+	//fmt.Printf("coeffs_qx: %v; coeffs_qz: %v\n", coeffs_qx, coeffs_qz)
 	if env.A == 0.0 {
 		return []float64{coeffs_qx[0], coeffs_qz[0], coeffs_qx[2], coeffs_qx[1], coeffs_qz[1], coeffs_qx[2]}, nil
 	} else {
--- tempLow/omegaPair_test.go
+++ tempLow/omegaPair_test.go
@@ -6,14 +6,14 @@ import (
 )
 import "../tempCrit"
 
-func TestOmegaPairCoeffs(t *testing.T) {
+func TestOmegaPairCoeffs_ZeroAtTc(t *testing.T) {
 	eps := 1e-6
 	env, err := lowDefaultEnv()
 	if err != nil {
 		t.Fatal(err)
 	}
 	// Find T = Tc omega_+ spectrum:
-	F0, Beta := env.F0, env.Beta // cache env
+	//F0, Beta := env.F0, env.Beta // cache env
 	env.F0 = 0.0                 // F0 is 0 at Tc
 	_, err = tempCrit.CritTempSolve(env, eps, eps)
 	if err != nil {
@@ -24,13 +24,13 @@ func TestOmegaPairCoeffs(t *testing.T) {
 		t.Fatal(err)
 	}
 	env.A, env.B = omegaFit[0], omegaFit[2]
-	fmt.Printf("got Beta_c = %f, A = %f, B = %f\n", env.Beta, env.A, env.B)
-	env.F0, env.Beta = F0, Beta
+	fmt.Printf("got Beta_c = %v, A = %v, B = %v\n", env.Beta, env.A, env.B)
+	//env.F0, env.Beta = F0, Beta
 	// Find T < Tc omega_+ spectrum.
-	_, err = D1MuSolve(env, eps, eps)
-	if err != nil {
-		t.Fatal(err)
-	}
+	//_, err = D1MuSolve(env, eps, eps)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
 	coeffs, err := OmegaFit(env, Omega_pp, eps, eps)
 	if err != nil {
 		t.Fatal(err)
